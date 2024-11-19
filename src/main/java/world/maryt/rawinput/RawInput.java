ad5e1954d73019ad15c9a14048565d7882e90f94
LitnhJacuzzi <2075397019@qq.com>
2024-11-19 22:02:35 +0800

    Commit for push.

---
+++ src/main/java/world/maryt/rawinput/RawInput.java
@@ -0,0 +1,44 @@
+package world.maryt.rawinput;
+
+import world.maryt.rawinput.commands.RescanCommand;
+import world.maryt.rawinput.commands.ToggleCommand;
+import world.maryt.rawinput.keybinds.KeybindHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = RawInput.MODID, version = RawInput.VERSION, name = RawInput.NAME, acceptedMinecraftVersions = "[1.11.2]")
+public class RawInput
+{
+    public static final String MODID = "rawinput";
+	public static final String NAME = "Raw Mouse Input";
+    public static final String VERSION = "1.6.2";
+	public static final Logger LOGGER = LogManager.getLogger(NAME);
+
+
+	@SideOnly(Side.CLIENT)
+	@EventHandler
+    public void init(FMLInitializationEvent event) {
+		ClientCommandHandler.instance.registerCommand(new RescanCommand());
+		ClientCommandHandler.instance.registerCommand(new ToggleCommand());
+		Minecraft.getMinecraft().mouseHelper = new RawMouseHelper();
+		MinecraftForge.EVENT_BUS.register(new KeybindHandler());
+		MinecraftForge.EVENT_BUS.register(new RawInputHandler());
+
+		RawInputHandler.init();
+    }
+
+	@SideOnly(Side.CLIENT)
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		KeybindHandler.init();
+	}
+}
---
+++ src/main/java/world/maryt/rawinput/commands/RescanCommand.java
@@ -0,0 +1,31 @@
+package world.maryt.rawinput.commands;
+
+import world.maryt.rawinput.RawInputHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
+
+public class RescanCommand extends CommandBase {
+	@Override
+	public String getName() {
+		return "rescan";
+	}
+
+	@Override
+	public String getUsage(ICommandSender sender) {
+		return "Rescans input devices: /rescan";
+	}
+
+	@Override
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		RawInputHandler.getMouse("Rescan Command Fired");
+		Minecraft.getMinecraft().player.sendMessage(new TextComponentString("Rescanned!"));
+	}
+	@Override
+	public int getRequiredPermissionLevel() {
+		return -1;
+	}
+}
